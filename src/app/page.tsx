2b8200a6a3fbe57d46adbb28dae30750083f92a2
MatheusDeZottis <matheusdezottis@gmail.com>
2024-07-16 08:48:52 -0300

    Testando componetes

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+        <div>
+          <h1>Masterrclass next</h1>
+          <nav>
+            
+          </nav>
+        </div>
+  
+    
+ 
+  )
+}
